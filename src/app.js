53bf0731563b637d402e6a8b8a7f0d975dab0083
ryuk@edyrrg <edyrojas64@gmail.com>
2022-10-04 12:07:28 -0600

    add app.js for server and implement api

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express'),
+    app = express(),
+    port = 3000,
+    root = '/api/delay/'
+
+app.get(root + ':id', (req, res) => {
+    let timer = Number(req.params.id)
+    if (Number.isInteger(timer)) {
+        setTimeout(() => { res.send(`after ${timer}s, CoreCode response: Hello World`) }, timer)
+    }
+})
+
+app.get(root, (req, res) => {
+    setTimeout(() => { res.send(`default after 1000s, CoreCode response: Hello`) }, 1000);
+})
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
